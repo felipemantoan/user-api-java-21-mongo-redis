3dbcfc853881c6ce9ac16f776dda2c3bb1f8e9dd
felipemantoan <luisfelipemantoan@gmail.com>
2025-01-25 11:33:06 -0300

    Habilita repositórios do mongoDb e redis, cria volumes

--- src/main/java/com/github/felipemantoan/user_api/UserApiApplication.java
+++ src/main/java/com/github/felipemantoan/user_api/UserApiApplication.java
@@ -2,8 +2,12 @@ package com.github.felipemantoan.user_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
+@EnableRedisRepositories
 public class UserApiApplication {
 
 	public static void main(String[] args) {
