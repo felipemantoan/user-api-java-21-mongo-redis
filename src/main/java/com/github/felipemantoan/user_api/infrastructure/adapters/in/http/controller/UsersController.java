0683b1b4493deef905523477a19dfe7f2cc173c3
felipemantoan <luisfelipemantoan@gmail.com>
2025-01-27 17:07:39 -0300

    Testando paginação

--- src/main/java/com/github/felipemantoan/user_api/infrastructure/adapters/in/http/controller/UsersController.java
+++ src/main/java/com/github/felipemantoan/user_api/infrastructure/adapters/in/http/controller/UsersController.java
@@ -82,7 +82,7 @@ public class UsersController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<UserResponseDTO>> getAll(@PageableDefault Pageable pageable) {
+    public ResponseEntity<Page<UserResponseDTO>> getAll(@PageableDefault(size = 100, sort = "created_at") Pageable pageable) {
         return ResponseEntity.ok(
             getAllUsersUseCase.execute(pageable).map(userHttpMapper::map)
         );
