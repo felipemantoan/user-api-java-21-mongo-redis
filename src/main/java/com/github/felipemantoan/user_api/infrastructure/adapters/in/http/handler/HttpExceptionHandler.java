ce22c3bde770ec27e38d76aca1250155555a100e
felipemantoan <luisfelipemantoan@gmail.com>
2025-02-08 11:47:41 -0300

    Deixando tudo minimante organizado e bonitinho

--- src/main/java/com/github/felipemantoan/user_api/infrastructure/adapters/in/http/handler/HttpExceptionHandler.java
+++ src/main/java/com/github/felipemantoan/user_api/infrastructure/adapters/in/http/handler/HttpExceptionHandler.java
@@ -1,11 +1,9 @@
 package com.github.felipemantoan.user_api.infrastructure.adapters.in.http.handler;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
@@ -15,8 +13,10 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import com.github.felipemantoan.user_api.domain.exceptions.UserConstraintValidationException;
 import com.github.felipemantoan.user_api.domain.exceptions.UserNotFoundException;
+import com.github.felipemantoan.user_api.infrastructure.adapters.in.http.dto.response.ErrorValidationResponseDTO;
+import com.github.felipemantoan.user_api.infrastructure.adapters.in.http.dto.response.UserErrorValidationResponseDTO;
+import com.github.felipemantoan.user_api.infrastructure.adapters.in.http.dto.response.UserNotFoundResponseDTO;
 
-import jakarta.validation.Path;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
@@ -24,34 +24,35 @@ import lombok.extern.log4j.Log4j2;
 public class HttpExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
-    ProblemDetail handleUserNotFoundException(UserNotFoundException e) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getLocalizedMessage());
-        problemDetail.setTitle(HttpStatus.NOT_FOUND.getReasonPhrase());
-        problemDetail.setDetail(e.getMessage());
-        problemDetail.setProperty("timestamp", Instant.now());
-        return problemDetail;
- 
+    ResponseEntity<UserNotFoundResponseDTO> handleUserNotFoundException(UserNotFoundException e) {
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(new UserNotFoundResponseDTO(HttpStatus.NOT_FOUND.value(), e.getMessage(), LocalDateTime.now()));
     }
 
     @ExceptionHandler(UserConstraintValidationException.class)
-    ResponseEntity<?> handleUserConstraintValidationException(UserConstraintValidationException e) {
-
-        List<Map<String, String>> errors = new ArrayList<>();
+    ResponseEntity<UserErrorValidationResponseDTO> handleUserConstraintValidationException(UserConstraintValidationException e) {
+        
+        List<ErrorValidationResponseDTO> errors = new ArrayList<>();
 
         e.getErrors().forEach(error -> {
-
-            Path property = error.getPropertyPath();
+            String property = error.getPropertyPath().toString();
             String parameter = error.getInvalidValue().toString();
-
-            Map<String, String> details = Map.of(
-                "message", error.getMessage(),
-                "field", property.toString(),
-                "value", parameter.toString()
-            );
-            errors.add(details);
+            
+            errors.add(new ErrorValidationResponseDTO(
+                error.getMessage(), 
+                property, 
+                parameter
+            ));
         });
 
+        UserErrorValidationResponseDTO response = new UserErrorValidationResponseDTO(
+            HttpStatus.BAD_REQUEST.value(), 
+            HttpStatus.BAD_REQUEST.name(), 
+            errors, 
+            LocalDateTime.now()
+        );
 
-        return ResponseEntity.badRequest().body(errors);
+        return ResponseEntity.badRequest().body(response);
     }
 }
