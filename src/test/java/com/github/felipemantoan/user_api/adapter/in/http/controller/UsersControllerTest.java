287a45b889224870b5dd65d2bb849d4dbb9961b1
felipemantoan <luisfelipemantoan@gmail.com>
2025-02-13 07:46:02 -0300

    Mais um teste

--- src/test/java/com/github/felipemantoan/user_api/adapter/in/http/controller/UsersControllerTest.java
+++ src/test/java/com/github/felipemantoan/user_api/adapter/in/http/controller/UsersControllerTest.java
@@ -1,9 +1,13 @@
 package com.github.felipemantoan.user_api.adapter.in.http.controller;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
+
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,7 +22,10 @@ import com.github.felipemantoan.user_api.application.port.in.DeleteUserPort;
 import com.github.felipemantoan.user_api.application.port.in.GetAllUsersPort;
 import com.github.felipemantoan.user_api.application.port.in.GetUserByIdPort;
 import com.github.felipemantoan.user_api.application.port.in.UpdateUserPort;
+import com.github.felipemantoan.user_api.domain.entity.User;
 import com.github.felipemantoan.user_api.infrastructure.adapter.in.http.controller.UsersController;
+import com.github.felipemantoan.user_api.infrastructure.adapter.in.http.dto.request.CreateUserRequestDTO;
+import com.github.felipemantoan.user_api.infrastructure.adapter.in.http.dto.response.UserResponseDTO;
 import com.github.felipemantoan.user_api.infrastructure.adapter.in.http.mapper.UserHttpMapper;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,6 +51,42 @@ public class UsersControllerTest {
     @Mock
     private UserHttpMapper userHttpMapper;
  
+    @Test
+    public void testCreate() {
+        
+        User user = User.builder().build();
+
+        CreateUserRequestDTO createUserRequestDTO = new CreateUserRequestDTO(
+            "dino", 
+            "12345678909", 
+            "email@email.com", 
+            "12345098760"
+        );
+   
+        UserResponseDTO responseDTO = new UserResponseDTO(
+            ObjectId.get().toString(),
+            "dino", 
+            "12345678909", 
+            "email@email.com", 
+            "12345098760", 
+            LocalDateTime.now(), 
+            LocalDateTime.now()
+        );
+        
+        when(userHttpMapper.map(any(CreateUserRequestDTO.class))).thenReturn(user);
+        when(createUserPort.execute(any(User.class))).thenReturn(user);
+        when(userHttpMapper.map(user)).thenReturn(responseDTO);
+
+        ResponseEntity<UserResponseDTO> response = controller.create(createUserRequestDTO);
+
+        Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        Assertions.assertEquals(responseDTO, response.getBody());
+
+        verify(userHttpMapper).map(any(CreateUserRequestDTO.class));
+        verify(createUserPort).execute(any(User.class));
+        verify(userHttpMapper).map(any(User.class));
+    }
+
     @Test
     public void testDelete() {
         ResponseEntity<Void> reponse = controller.delete("xpto");
