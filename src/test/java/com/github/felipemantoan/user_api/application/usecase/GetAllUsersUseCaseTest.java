88693b91c92bd2c788ca56c43f15bd7baf469146
felipemantoan <luisfelipemantoan@gmail.com>
2025-02-15 11:22:44 -0300

    Finaliza mais um teste

---
+++ src/test/java/com/github/felipemantoan/user_api/application/usecase/GetAllUsersUseCaseTest.java
@@ -0,0 +1,34 @@
+package com.github.felipemantoan.user_api.application.usecase;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import com.github.felipemantoan.user_api.application.port.out.database.UserServiceDatabasePort;
+
+@ExtendWith(MockitoExtension.class)
+public class GetAllUsersUseCaseTest {
+    
+    @Mock
+    private UserServiceDatabasePort port;
+
+    @InjectMocks
+    private GetAllUsersUseCase useCase;
+
+    @Test
+    public void testExecuteSuccess() {
+        when(port.getAll(any(Pageable.class))).thenReturn(Page.empty());
+        useCase.execute(Pageable.unpaged());
+        verify(port).getAll(any(Pageable.class));
+    }
+
+}
