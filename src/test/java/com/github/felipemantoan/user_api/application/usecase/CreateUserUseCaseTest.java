de094469b0e603f4d466795b0c80e381e2b665a5
felipemantoan <luisfelipemantoan@gmail.com>
2025-02-15 10:39:35 -0300

    Incluindo mais um cenário de testes

--- src/test/java/com/github/felipemantoan/user_api/application/usecase/CreateUserUseCaseTest.java
+++ src/test/java/com/github/felipemantoan/user_api/application/usecase/CreateUserUseCaseTest.java
@@ -1,8 +1,14 @@
 package com.github.felipemantoan.user_api.application.usecase;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Set;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,7 +17,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.github.felipemantoan.user_api.application.port.out.database.UserServiceDatabasePort;
 import com.github.felipemantoan.user_api.domain.entity.User;
+import com.github.felipemantoan.user_api.domain.exception.UserConstraintValidationException;
 
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,8 +39,27 @@ public class CreateUserUseCaseTest {
         User user = User.builder().build();
 
         when(port.save(any(User.class))).thenReturn(user);
+        when(validator.validate(any(User.class))).thenReturn(Set.of());
 
-        User createdUser = useCase.execute(user);
+        useCase.execute(user);
+
+        verify(validator).validate(any(User.class));
+        verify(port).save(any(User.class));
+    }
+
+    @Test
+    public void testExecuteError() {
+        User user = User.builder().build();
+        ConstraintViolation mockConstraintViolation = mock(ConstraintViolation.class);
+        
+        when(validator.validate(any(User.class))).thenReturn(Set.of(mockConstraintViolation));
+
+        Assertions.assertThrows(UserConstraintValidationException.class, () -> {
+            useCase.execute(user);
+        });
+
+        verify(validator).validate(any(User.class));
+        verify(port, times(0)).save(any(User.class));
     }
 
 }
