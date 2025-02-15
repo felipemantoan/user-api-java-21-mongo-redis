c3981b665e78e2dc93cfdceadb9bf765ccb02015
felipemantoan <luisfelipemantoan@gmail.com>
2025-02-15 09:17:20 -0300

    Mais um testezinho

--- src/test/java/com/github/felipemantoan/user_api/adapter/in/http/controller/UsersControllerTest.java
+++ src/test/java/com/github/felipemantoan/user_api/adapter/in/http/controller/UsersControllerTest.java
@@ -2,10 +2,12 @@ package com.github.felipemantoan.user_api.adapter.in.http.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Assertions;
@@ -14,6 +16,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -26,6 +31,7 @@ import com.github.felipemantoan.user_api.domain.entity.User;
 import com.github.felipemantoan.user_api.infrastructure.adapter.in.http.controller.UsersController;
 import com.github.felipemantoan.user_api.infrastructure.adapter.in.http.dto.request.CreateUserRequestDTO;
 import com.github.felipemantoan.user_api.infrastructure.adapter.in.http.dto.request.UpdateUserRequestDTO;
+import com.github.felipemantoan.user_api.infrastructure.adapter.in.http.dto.response.PageableUserResponseDTO;
 import com.github.felipemantoan.user_api.infrastructure.adapter.in.http.dto.response.UserResponseDTO;
 import com.github.felipemantoan.user_api.infrastructure.adapter.in.http.mapper.UserHttpMapper;
 
@@ -118,6 +124,51 @@ public class UsersControllerTest {
         verify(updateUserPort).execute(anyString(), anyString(), anyString(), anyString());
         verify(userHttpMapper).map(any(User.class));
 
+    }
+
+    @Test
+    public void testGetById() {
+        String userId = ObjectId.get().toString();
+        User user = User.builder().id(userId).build();
+        
+        UserResponseDTO responseDTO = new UserResponseDTO(
+            ObjectId.get().toString(),
+            "dino", 
+            "12345678909", 
+            "email@email.com", 
+            "12345098760", 
+            LocalDateTime.now(), 
+            LocalDateTime.now()
+        );
+
+        when(getUserByIdPort.execute(anyString())).thenReturn(user);
+        when(userHttpMapper.map(user)).thenReturn(responseDTO);
+
+        ResponseEntity<UserResponseDTO> response = controller.getById(userId);
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        verify(getUserByIdPort).execute(userId);
+    }
+
+    @Test
+    public void testGetAll() {
+        PageableUserResponseDTO pageableUserResponseDTO = new PageableUserResponseDTO(
+            null, 
+            0, 
+            0, 
+            false, 
+            false, 
+            0, 
+            0, 
+            0, 
+            true
+        );
+        Page<User> page = new PageImpl<>(List.of(User.builder().build()));
+        when(getAllUsersPort.execute(any(Pageable.class))).thenReturn(page);
+        when(userHttpMapper.map(page)).thenReturn(pageableUserResponseDTO);
+        ResponseEntity<PageableUserResponseDTO> response = controller.getAll(Pageable.unpaged());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        verify(getAllUsersPort).execute(any(Pageable.class));
     }
 
     @Test
