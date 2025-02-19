59bf6ba7f2c47862eaba7e24242a3eace4360757
felipemantoan <luisfelipemantoan@gmail.com>
2025-02-19 17:52:50 -0300

    Mais um cenário

--- src/test/java/com/github/felipemantoan/user_api/application/usecase/UpdateUserUseCaseTest.java
+++ src/test/java/com/github/felipemantoan/user_api/application/usecase/UpdateUserUseCaseTest.java
@@ -3,10 +3,12 @@ package com.github.felipemantoan.user_api.application.usecase;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
@@ -20,6 +22,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.github.felipemantoan.user_api.application.port.out.database.UserServiceDatabasePort;
 import com.github.felipemantoan.user_api.domain.entity.User;
+import com.github.felipemantoan.user_api.domain.exception.UserConstraintValidationException;
 import com.github.felipemantoan.user_api.domain.exception.UserNotFoundException;
 
 import jakarta.validation.ConstraintValidator;
@@ -69,7 +72,7 @@ public class UpdateUserUseCaseTest {
     }
 
     @Test
-    public void testExecuteUserNotFound() {
+    public void testExecuteUserNotFoundException() {
 
         when(port.getOne(anyString())).thenReturn(Optional.empty());
 
@@ -80,6 +83,37 @@ public class UpdateUserUseCaseTest {
         });
 
         verify(port).getOne(anyString());
+    }
+
+    @Test
+    public void testExecuteUserConstraintValidationException() {
+
+        String userId = ObjectId.get().toString();
+    
+        User user = User.builder()
+            .id(userId)
+            .name("lucifer")
+            .cpf("76343098852")
+            .phoneNumber("1988887777")
+            .email("email@email.com")
+            .createdAt(LocalDateTime.of(2024, 12, 31, 23, 59))
+            .updatedAt(LocalDateTime.of(2025, 02, 02, 23, 59))
+        .build();
+
+        ConstraintViolation<User> mockConstraintViolation = mock(ConstraintViolation.class);
+        Set<ConstraintViolation<User>> errors = new HashSet<ConstraintViolation<User>>();
+        errors.add(mockConstraintViolation);
+
+        when(validator.validate(any(User.class))).thenReturn(errors);
+        when(port.getOne(anyString())).thenReturn(Optional.of(user));
+
+        Assertions.assertThrows(UserConstraintValidationException.class, () -> {
+            useCase.execute(userId, "lucifer", "email@email.com", "1988887777");
+        });
+
+        verify(port, times(0)).save(any(User.class));
+        verify(port).getOne(anyString());
+        verify(validator).validate(any(User.class));
     }
 
 }
