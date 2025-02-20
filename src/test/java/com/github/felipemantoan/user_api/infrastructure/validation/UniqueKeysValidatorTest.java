b8249d6f43bee2558eafcbb2754eadcef501e38d
felipemantoan <luisfelipemantoan@gmail.com>
2025-02-20 18:26:31 -0300

    Muita escrita

--- src/test/java/com/github/felipemantoan/user_api/infrastructure/validation/UniqueKeysValidatorTest.java
+++ src/test/java/com/github/felipemantoan/user_api/infrastructure/validation/UniqueKeysValidatorTest.java
@@ -1,6 +1,7 @@
 package com.github.felipemantoan.user_api.infrastructure.validation;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -21,6 +22,8 @@ import org.springframework.data.mongodb.core.query.Query;
 import com.mongodb.assertions.Assertions;
 
 import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.ConstraintValidatorContext.ConstraintViolationBuilder;
+import jakarta.validation.ConstraintValidatorContext.ConstraintViolationBuilder.NodeBuilderCustomizableContext;
 import lombok.Data;
 
 @ExtendWith(MockitoExtension.class)
@@ -61,7 +64,7 @@ public class UniqueKeysValidatorTest {
     }
 
     @Test
-    public void testUniqueKeysValidatorIsValid() {
+    public void testUniqueKeysValidatorIsValidTrue() {
 
         when(annotation.id()).thenReturn("id");
         when(annotation.mongoId()).thenReturn("_id");
@@ -90,6 +93,46 @@ public class UniqueKeysValidatorTest {
         Assertions.assertTrue(validator.isValid(innerClass, context));
 
         verify(context, times(0)).disableDefaultConstraintViolation();
+    }
+
+    @Test
+    public void testUniqueKeysValidatorIsValidFalse() {
+
+        when(annotation.id()).thenReturn("id");
+        when(annotation.mongoId()).thenReturn("_id");
+        when(annotation.keys()).thenReturn(new String[]{"email", "username"});
+        
+        validator.initialize(annotation);
+
+        InnerUniqueKeys innerClass = new InnerUniqueKeys();
+
+        innerClass.setId(ObjectId.get().toString());
+        innerClass.setEmail("email@email.com");
+        innerClass.setUsername("ghost-user");
+
+        when(template.getCollectionName(InnerUniqueKeys.class)).thenReturn("stub");
+
+        TerminatingFind terminatingFind = mock(TerminatingFind.class);
+        when(terminatingFind.exists()).thenReturn(Boolean.TRUE);
+        
+        ExecutableFind executableFind = mock(ExecutableFind.class);
+        when(executableFind.matching(any(Query.class))).thenReturn(terminatingFind);
+        
+        when(template.query(InnerUniqueKeys.class)).thenReturn(executableFind);
+
+        ConstraintValidatorContext context = mock(ConstraintValidatorContext.class);
+
+        NodeBuilderCustomizableContext node = mock(NodeBuilderCustomizableContext.class);
+        when(node.addConstraintViolation()).thenReturn(context);
+        
+        ConstraintViolationBuilder builder = mock(ConstraintViolationBuilder.class);
+        when(builder.addPropertyNode(anyString())).thenReturn(node);
+        
+        when(context.buildConstraintViolationWithTemplate(anyString())).thenReturn(builder);
+        
+        Assertions.assertFalse(validator.isValid(innerClass, context));
+
+        verify(context, times(1)).disableDefaultConstraintViolation();
     }
 
     /**
