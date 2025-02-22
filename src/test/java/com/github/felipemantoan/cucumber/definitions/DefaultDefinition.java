7e1e93c00f6fdf94127b581afe9415973ffd969e
felipemantoan <luisfelipemantoan@gmail.com>
2025-02-22 13:53:49 -0300

    Incluindo frases iniciais

--- src/test/java/com/github/felipemantoan/cucumber/definitions/DefaultDefinition.java
+++ src/test/java/com/github/felipemantoan/cucumber/definitions/DefaultDefinition.java
@@ -2,7 +2,28 @@ package com.github.felipemantoan.cucumber.definitions;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DefaultDefinition {
     
+    @Given("I created a new user")
+    public void i_created_a_new_user() {
+        // Write code here that turns the phrase above into concrete actions
+        // throw new io.cucumber.java.PendingException();
+    }
+    @When("I see a user created")
+    public void i_see_a_user_created() {
+        // Write code here that turns the phrase above into concrete actions
+        // throw new io.cucumber.java.PendingException();
+    }
+    @Then("I should be cpf filled")
+    public void i_should_be_cpf_filled() {
+        // Write code here that turns the phrase above into concrete actions
+        // throw new io.cucumber.java.PendingException();
+    }
+
+
 }
