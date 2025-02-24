3abc59577f8550d19131d13e87e0a6aa685c0445
felipemantoan <luisfelipemantoan@gmail.com>
2025-02-23 21:58:52 -0300

    Apaga código inutil em favor do uso de annotations

--- src/main/java/com/github/felipemantoan/user_api/UserApiApplication.java
+++ src/main/java/com/github/felipemantoan/user_api/UserApiApplication.java
@@ -6,10 +6,14 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @EnableCaching
 @EnableMongoRepositories
 @EnableMongoAuditing
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "User API", version = "1.0", description = "API for managing users"))
 public class UserApiApplication {
 
 	public static void main(String[] args) {
